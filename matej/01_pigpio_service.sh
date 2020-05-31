4947b7b6f1357dfb3f51904bdb075913572f3565
TheoryX <51214627+mitpre@users.noreply.github.com>
2020-05-31 19:30:53 +0200

    Update 01_pigpio_service.sh

--- matej/01_pigpio_service.sh
+++ matej/01_pigpio_service.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# script name:     conf_service.sh
-# last modified:   2018/08/12
-# credits:         mt08xx
-# sudo:            yes
 
 script_name=$(basename -- "$0")
 
@@ -12,17 +8,8 @@ if ! [ $(id -u) = 0 ]; then
 fi
 
 if [ $SUDO_USER ]; then usr=$SUDO_USER; else usr=`whoami`; fi
-env="/home/${usr}/.venv/jns"
 
-
-# create pigpio_daemon_start.sh in /home/<user> and make it executable
-cat << ONE > /home/${usr}/pigpio_daemon_start.sh && chmod a+x /home/${usr}/pigpio_daemon_start.sh
-#!/bin/bash
-source ${env}/bin/activate
-pigpiod -n localhost
-ONE
-
-cat << TWO | sudo tee /etc/systemd/system/pigpiodaemon.service
+cat << ONE | sudo tee /etc/systemd/system/pigpiodaemon.service
 [Unit]
 Description=Daemon required to control GPIO pins via pigpio
 
@@ -33,9 +20,9 @@ Type=forking
 
 [Install]
 WantedBy=multi-user.target
-TWO
+ONE
 
-# start jupyter
 systemctl daemon-reload
 systemctl enable pigpiodaemon.service
 systemctl start pigpiodaemon.service
+#systemctl status pigpiodaemon.service

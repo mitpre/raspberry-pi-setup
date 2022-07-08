6c691ede2956ec15430b8a8e3156eb85e5538e07
TheoryX <51214627+mitpre@users.noreply.github.com>
2022-07-08 16:33:03 +0200

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -4,6 +4,7 @@
 # sudo: no
 
 RED='\033[0;31m'
+GREEN='\033[0;33m'
 NC='\033[0m' # No Color
 
 script_name=$(basename -- "$0")
@@ -16,21 +17,33 @@ then
    exit 1
 fi
 
-echo -e "${RED}Upgrading 'pip'${NC}"
+echo -e "${GREEN}Upgrading 'pip'${NC}"
 python3 -m pip install --upgrade pip
 
-echo -e "${RED}Install 'pipx'${NC}"
+echo -e "${GREEN}Install 'pipx'${NC}"
 python3 -m pip install --user pipx
 python3 -m pipx ensurepath
 source ~/.bashrc
 # pipx completetions
 echo 'eval "$(register-python-argcomplete pipx)"' > ~/.bashrc
 
-echo -e "${RED}Install 'virtualenv'${NC}"
+echo -e "${GREEN}Install 'virtualenv'${NC}"
 pipx install virtualenv
 
-echo -e "${RED}Install 'Node.js'${NC}"
+echo -e "${GREEN}Install 'Node.js'${NC}"
 curl -L https://bit.ly/n-install | bash -s -- -y lts
 
-echo -e "${RED}Install 'pigpio'${NC}"
+echo -e "${GREEN}Install 'pigpio'${NC}"
 sudo apt-get install pigpio python3-pigpio
+
+env="/home/${usr}/.venv/jupyter"
+if [ ! -d "$venv" ]; then
+   echo -e "${GREEN}Create virtual environment for 'jupyter'${NC}"
+  virtualenv $env
+fi
+
+env="/home/${usr}/.venv/shades"
+if [ ! -d "$venv" ]; then
+   echo -e "${GREEN}Create virtual environment for 'shades'${NC}"
+  virtualenv $env
+fi

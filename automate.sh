9a504812813cbf4759a7988759ce410a45e3cf84
TheoryX <51214627+mitpre@users.noreply.github.com>
2022-07-08 16:39:46 +0200

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -37,13 +37,16 @@ echo -e "${GREEN}Install 'pigpio'${NC}"
 sudo apt-get install pigpio python3-pigpio
 
 env="/home/${usr}/.venv/jupyter"
-if [ ! -d "$venv" ]; then
+if [ ! -d "$env" ]; then
    echo -e "${GREEN}Create virtual environment for 'jupyter'${NC}"
   virtualenv $env
+else
+   echo -e "${GREEN}'jupyter' virtual environment already exists${NC}"
 fi
-
 env="/home/${usr}/.venv/shades"
-if [ ! -d "$venv" ]; then
+if [ ! -d "$env" ]; then
    echo -e "${GREEN}Create virtual environment for 'shades'${NC}"
   virtualenv $env
+else
+   echo -e "${GREEN}'shades' virtual environment already exists${NC}"
 fi
